b2106af66ea15a1a6c3068de722cb9b5e4cd7170
llldddbbb <571002217@qq.com>
2017-04-11 00:59:04 +0800

    first commit

---
+++ src/main/java/com/example/UEditorController.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import com.baidu.ueditor.ActionEnter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by ldb on 2017/4/9.
+ */
+@Controller
+public class UEditorController {
+
+
+    @RequestMapping("/")
+    private String showPage(){
+        return "index";
+    }
+
+    @RequestMapping(value="/config")
+    public void config(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("application/json");
+        String rootPath = request.getSession().getServletContext().getRealPath("/");
+        try {
+            String exec = new ActionEnter(request, rootPath).exec();
+            PrintWriter writer = response.getWriter();
+            writer.write(exec);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/example/UeditorTestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UeditorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UeditorTestApplication.class, args);
+	}
+}
